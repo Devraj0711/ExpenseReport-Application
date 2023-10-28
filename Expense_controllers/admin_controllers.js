8929050f618f06c17d0cbdc777230f0f657431c9
Devraj0711 <69614639+Devraj0711@users.noreply.github.com>
2023-10-28 17:29:16 +0530

    update with Sigin page
    Sigin page is added with GET call included

--- Expense_controllers/admin_controllers.js
+++ Expense_controllers/admin_controllers.js
@@ -1,6 +1,9 @@
 const Home_page = require('../models/db_model');
 const path = require('path');
-exports.getIndex = (req, res, next) => {
+const { Op } = require('sequelize');
+
+//sigup page
+exports.getSignup = (req, res, next) => {
     Home_page.findAll()
     .then(rows => {
       res.sendFile(path.join(__dirname, '..', 'views', 'Home', 'signup.html'));
@@ -14,32 +17,46 @@ exports.getIndex = (req, res, next) => {
 
 exports.postAddDetails = (req, res, next) => {
     console.log("post command");
-    
-    const Username = req.body.Username.trim();
+
+    const Username = req.body.Username;
     const Email = req.body.Email;
     const Password = req.body.Password;
-    // req.user
-    // .createProduct({
-    //     Username: Username,
-    //     Email: Email,
-    //     Password: Password
-    // })
-    //further simplifing code to include association  
-    Home_page.create({
-      Username: Username,
-        Email: Email,
-        Password: Password,
-      // manually setting to associate User and Product table  //userId: req.user.id 
-    })
-    .then(result => {
-      console.log(result);
-      res.redirect('/signin'); // Redirect to a success page or wherever appropriate
-    })
-    .catch(err => {
-      console.log(err);
-      // Handle the error appropriately, e.g., send an error response or render an error page
-    });
-  };
+
+    // Check if the email already exists in the database
+    Home_page.findOne({
+        where: {
+            [Op.or]: [
+                { Email: Email },
+                { Username: Username }
+            ]
+        }
+        })
+        .then(existingUser => {
+            if (existingUser) {
+                // Email already exists, return 403 Forbidden status
+                return res.status(403).alert(json({ error: 'Details already exists' }));
+            } else {
+                // Email doesn't exist, create a new record
+                Home_page.create({
+                    Username: Username,
+                    Email: Email,
+                    Password: Password
+                })
+                .then(result => {
+                    console.log(result);
+                    res.redirect('/Home/signin'); // Redirect to a success page or wherever appropriate
+                })
+                .catch(err => {
+                    console.log(err);
+                    // Handle the error appropriately, e.g., send an error response or render an error page
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            // Handle the error appropriately, e.g., send an error response or render an error page
+        });
+};
 
 
 // exports.getIndex = (req, res, next) => {
@@ -55,3 +72,17 @@ exports.postAddDetails = (req, res, next) => {
 //     console.log(err);
 //   });
 // };
+
+//signin page
+
+exports.getSignin = (req, res, next) => {
+    Home_page.findAll()
+    .then(rows => {
+      res.sendFile(path.join(__dirname, '..', 'views', 'Home', 'signin.html'));
+      console.log(rows);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    });
+};
--- Expense_routes/admin_home.js
+++ Expense_routes/admin_home.js
@@ -8,7 +8,9 @@ const router = express.Router();
 
 // /admin/home => GET
 
-router.get('/', adminController.getIndex);
+router.get('/', adminController.getSignup);
 router.post('/Home/signup.html', adminController.postAddDetails);
+
+router.get('/Home/signin',adminController.getSignin);
 
 module.exports = router;
