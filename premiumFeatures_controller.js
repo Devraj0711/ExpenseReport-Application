772e0b08d63b119cf1de529a8dd637ec6bffc558
Devraj0711 <69614639+Devraj0711@users.noreply.github.com>
2023-11-11 14:44:40 +0530

    LeaderBoad Optimised

--- premiumFeatures_controller.js
+++ premiumFeatures_controller.js
@@ -10,68 +10,34 @@ const jwt= require('jsonwebtoken');
 const userController=  require('../Expense_controllers/admin_controllers');
 
 const Order =require('../models/order');
+const sequelize = require('../util/database');
 
 
 exports.showLeaderboard = async (req, res) => {
     try {
-        const users = await Home_page.findAll();
-        const expenses = await Expense.findAll();
-
-        // Create a dictionary to store aggregated expenses for each user
-        const userAggregatedExpenses = {};
+        // optimized code 
+        const leaderboardofUsers= await Home_page.findAll({
+            attributes: ['id', 'Username', [sequelize.fn('sum', sequelize.col('Amount')), 'total_cost']],
+            include: [
+                {
+                    model: Expense,
+                    attributes: []
+                }
+            ],
+            group: ['id'],
+            order:[['total_cost', 'DESC']]
+        })
+        res.status(200).json(leaderboardofUsers)
         
-        expenses.forEach((expense) => {
-            if (userAggregatedExpenses[expense.ExpenseReportId]) {
-                userAggregatedExpenses[expense.ExpenseReportId] += expense.Amount;
-            } else {
-                userAggregatedExpenses[expense.ExpenseReportId] = expense.Amount;
-            }
-        });
 
-        // Create an array to store user leaderboard details
-        const userLeaderboardDetails = users.map((user) => {
-            const totalCost = userAggregatedExpenses[user.id] || 0;
-            return { Username: user.Username, total_cost: totalCost };
-        });
-
-        console.log('total amounts --> ', userLeaderboardDetails);
-        userLeaderboardDetails.sort((a,b)=>b.total_cost - a.total_cost);
-        res.status(200).json(userLeaderboardDetails);
-    } catch (err) {
+    } 
+    catch (err) {
         console.error(err);
         res.status(500).json(err);
     }
 };
 
 
-// exports.showLeaderboard= async(req,res)=>{
-//     try{
-//         const users= await Home_page.findAll()
-//         const expenses= await Expense.findAll();
-//         const useraAggregatedExpenses = {};
-//         expenses.forEach((expense)=>{
-//             if(useraAggregatedExpenses[expense.ExpenseReportId])
-//             {
-//                 useraAggregatedExpenses[expense.ExpenseReportId]= useraAggregatedExpenses[expense.ExpenseReportId]+ expense.Amount;
-//             }
-//             else
-//             {
-//                 useraAggregatedExpenses[expense.ExpenseReportId]= expense.Amount;
-//             }
-//         }) 
-//         var userLeaderboardDetails= [];
-//         users.forEach((user)=>{
-//             userLeaderboardDetails.push({Username:user.Username, total_cost: useraAggregatedExpenses[user.id]})
-//         })
-//         console.log("total amounts --> ", userLeaderboardDetails);
-//         res.status(200).json(userLeaderboardDetails);
 
-//     }
-//     catch(err)
-//     {
-//         console.log(err)
-//         res.status(500).json(err)
-//     }
-// }
 
 
