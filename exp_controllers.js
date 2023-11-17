9d656b3d7c2ca320103081334ae9feabb288e3d3
Devraj0711 <69614639+Devraj0711@users.noreply.github.com>
2023-11-17 18:31:26 +0530

    Download feature added.
    when the download button is clicked, text file is downloaded.

--- exp_controllers.js
+++ exp_controllers.js
@@ -1,5 +1,6 @@
 const Home_page = require('../models/db_model'); // Adjust the path based on your project structure
 const Exp_page = require('../models/ExpenseDB'); // Adjust the path based on your project structure
+const StoreURL= require('../models/StoreURL');
 
 const token_ans= require('../Expense_controllers/admin_controllers'); // for export ans
 
@@ -12,6 +13,7 @@ const { Op } = require('sequelize');
 const fs = require('fs');
 const bcrypt= require('bcrypt');
 
+const AWS= require('aws-sdk');
 
 // const token = jwt.sign({ ExpenseReportIdId: Exp_page.ExpenseReportId }, 'dvysis23', { expiresIn: '1h' }); // Sign the token
 const ansValue = token_ans.getAns();
@@ -36,7 +38,6 @@ exports.getIndex=(req, res, next) => {
       console.log(err);
     });
 }
-
 
 exports.postIndex = async (req, res, next) => {
   const t = await sequelize.transaction();
@@ -94,62 +95,60 @@ exports.postIndex = async (req, res, next) => {
   }
 };
 
+function uploadToS3(data, filename){
+  const BUCKET_NAME = 'expensetracker201077';
+  const IAM_USER_KEY = 'AKIAVWJNUYL7YMME3TY4';
+  const IAM_USER_SECRET = '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';
 
-// exports.postIndex = async(req, res, next) => {
-//   const t= await sequelize.transaction()
-//   const { Amount, description, Category } = req.body;
-//   const token = req.headers.authorization;
+  let s3bucket = new AWS.S3({
+    accessKeyId: IAM_USER_KEY,
+    secretAccessKey: IAM_USER_SECRET,
+    Bucket: BUCKET_NAME
+  })
+  
+    var params = {
+      Bucket: BUCKET_NAME,
+      Key: filename,
+      Body: data,
+      ACL: 'public-read'
+    }
 
-//   if (!token) {
-//     return res.status(401).json({ error: 'Unauthorized', success: false });
-//   }
-
-//   // Verify the token
-//   jwt.verify(token.replace('Bearer ', ''), 'dvysis23', (err, decodedToken) => {
-//     if (err) {
-//       console.error('Error decoding token: ', err);
-//       return res.status(401).json({ error: 'Unauthorized', success: false });
-//     }
-
-//   const ExpenseReportIdId  = decodedToken.ExpenseReportId;
-
-//   console.log("decoded ----", ExpenseReportIdId);
-
-//   Exp_page.create({
-//     Amount: Amount,
-//     description: description,
-//     Category: Category,
-//     ExpenseReportId: ExpenseReportIdId
-//   })
-//   .then(expenses => {
-
-//     //adding  total expense of a user in the totalExpenses column
-//     Home_page.findOne({where: {id: expenses.ExpenseReportId}}).then(user=> {
+    return new Promise((resolve, reject) => {
+      s3bucket.upload(params, (err, s3response) => {
+        if(err) {
+          console.log("Something went wrong", err)
+          reject(err);
+        }
+        else {
+          console.log('Success', s3response);
+          resolve(s3response.Location);
+        }
+      })
+   
+    })
     
-//     const totalExpense =Number(user.totalExpenses)+ Number(expenses.Amount);
-//     Home_page.update({
-//       totalExpenses: totalExpense
-//     }, {
-//       where: {id: expenses.ExpenseReportId},
-//       transaction: t
-//     }).then(async()=>{
-//       await t.commit();
-//       console.log(expenses);
-//       console.log("Let see the total Expenses  ", totalExpense);
-//       return res.status(201).json({ expense: expenses, success: true });
-//     })
-//     .catch(async(err)=>{
-//       await t.rollback();
-//       throw new Error(err);
-//   })
-// })
-//   .catch(err => {
-//     console.error('Error inserting data: ', err);
-//     return res.status(500).json({ error: 'Internal Server Error', success: false });
-//   })
-// })
-// .catch(err => {
-//   console.error('Error inserting data: ', err);
-//   })
-// })
-// }
+} //end of s3Upload function
+
+
+exports.downloadexpense = async(req, res) => {
+  try{
+    const userId= req.user.id;
+    
+    const expenses= await Exp_page.findOne({ where: { ExpenseReportId: userId} });;
+    console.log("Showing current user expenses ",expenses);
+    const stringifiedExpenses = JSON.stringify(expenses);
+
+    const filename= `Expense${userId}/${new Date()}.txt`;
+    const fileURL= await uploadToS3(stringifiedExpenses, filename);
+    const URL = await StoreURL.create({
+      URL: fileURL,
+      ExpenseReportId: userId 
+    });
+
+    res.status(200).json({fileURL, success: true})
+} catch(err){
+  console.log(err);
+  res.status(500).json({fileURL: '',success: false, err:err})
+}
+
+}
